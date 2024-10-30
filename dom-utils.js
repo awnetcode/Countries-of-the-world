2d05507e8dd4fcd5d3c39bb86b7b2bbfdeff427e
awnetcode <155958835+awnetcode@users.noreply.github.com>
2024-10-30 19:30:36 +0100

    data-injection-and-layout

--- dom-utils.js
+++ dom-utils.js
@@ -1,6 +1,46 @@
+const createInfoElement = (labelName, value) =>{
+    const infoElement = document.createElement("div");
+
+    const labelElement = document.createElement("strong");
+    labelElement.innerText = `${labelName}: `;
+    const valueElement = document.createElement("span");
+    valueElement.innerText = value;
+
+    infoElement.appendChild(labelElement);
+    infoElement.appendChild(valueElement);
+
+    return infoElement;
+};
+
+const createFlagItemElement = (country) =>{
+    const imgContainerElement = document.createElement("div");
+    const imgElement = document.createElement("img");
+    imgElement.src = country.flagUrl;
+
+    imgContainerElement.appendChild(imgElement);
+
+    return imgContainerElement;
+}
+
 const createCountryItemElement = (country) =>{
     const countryElement = document.createElement("li");
-    const couuntryName = document.createElement("span");
+    const countryNameElement = document.createElement("strong");
+    countryNameElement.classList.add("country-name");
+
+    countryNameElement.innerText = country.countryName;
+
+    countryElement.appendChild(createFlagItemElement(country));
+
+    const infoContainerElement = document.createElement("div");
+    infoContainerElement.classList.add("info-container");
+
+    infoContainerElement.appendChild(countryNameElement);
+    infoContainerElement.appendChild(createInfoElement("Population", country.population));
+    infoContainerElement.appendChild(createInfoElement("Region", country.region));
+    infoContainerElement.appendChild(createInfoElement("Capital", country.capital));
+
+    countryElement.appendChild(infoContainerElement);
+
     return countryElement;
 }
 
